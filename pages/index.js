c14e0ac45bee4703e985dbde4c1af55bf45cb68a
Vikram Chhapwale <veryseriousemailaddress@gmail.com>
2023-05-20 12:06:02 +0100

    Pre-rendering and Data Fetching (#4)
    * Fetch, parse Markdown to statically generate pages using getStaticProps
    
    * Commented out code to fetch code remotely, added comment to show the remote data fetching and how to fetch data at request time
    
    ---------
    
    Co-authored-by: Vikramaditya Chhapwale <cvikramad@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,18 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import Alert from "../components/alert";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home ({ allPostsData }) { 
   return (
     <Layout home>
       <Head>
@@ -12,12 +22,19 @@ export default function Home() {
 
       <Alert type={"success"}>This blog is live</Alert>
       {/* This is how you use clsx  - pick the class via type which can be passed as a JS Variable */}
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
