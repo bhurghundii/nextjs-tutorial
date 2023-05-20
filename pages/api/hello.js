a95fae7ce456d2bca11b9411905b460726d10e89
Vikram Chhapwale <veryseriousemailaddress@gmail.com>
2023-05-20 13:01:04 +0100

    Add an API (#6)
    Co-authored-by: Vikramaditya Chhapwale <cvikramad@gmail.com>

---
+++ pages/api/hello.js
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+    res.status(200).json({ text: 'Hello' });
+}
+
+// More docs here: https://nextjs.org/learn/basics/api-routes/api-routes-details 
+// These are SERVER SIDE
